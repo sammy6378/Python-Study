39fc2090c8b111a1c37d6f57ce5e0695ebbebd61
sammy6378 <samuel202mwangi@gmail.com>
2024-11-07 17:16:43 +0300

    code: add week 1 assignment

---
+++ week 1/user_input.py
@@ -0,0 +1,8 @@
+
+# Asking for user's input
+name = input("Please enter your name: ")
+age = input("Please enter your age: ")
+location = input("Please enter your location: ")
+
+# Printing a personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
